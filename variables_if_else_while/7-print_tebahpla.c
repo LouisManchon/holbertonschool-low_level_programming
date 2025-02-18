032cebffd62d73d84f82eb2aab32dcb0ffb42435
LouisManchon <manchon.louis31@gmail.com>
2025-02-18 17:10:11 +0100

    prints the lowercase alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -  prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 122 ; a >= 97 ; a--)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
