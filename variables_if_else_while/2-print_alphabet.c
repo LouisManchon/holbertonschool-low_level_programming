020be99d383d13ede6cabeb176a971f1ff901715
LouisManchon <manchon.louis31@gmail.com>
2025-02-18 16:15:20 +0100

    program that prints the alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -  prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97 ; a < 123 ; a++)
+		putchar(a);
+
+	putchar('\n');
+
+}
