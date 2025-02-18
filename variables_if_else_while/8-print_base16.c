3410237f6523d7cb0c2d79df94b8aa4ab54c02df
LouisManchon <manchon.louis31@gmail.com>
2025-02-18 17:26:21 +0100

    prints all the numbers of base 16 in lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -  prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48 ; a <= 57 ; a++)
+		putchar(a);
+
+	for (a = 97 ; a <= 102 ; a++)
+	       	putchar(a)
+
+	putchar('\n');
+
+	return (0);
+}
