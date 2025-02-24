168ab351be381fd0e4f140588a2697f7e5329b85
LouisManchon <manchon.louis31@gmail.com>
2025-02-24 15:31:38 +0100

    Write a function that prints the numbers, from 0 to 9, followed by a new line except 2 and 4

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _putchar();
+void _putchar(char c);
 
 int _isupper(int c);
 
@@ -10,5 +10,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 
 void print_numbers(void);
+
+void print_most_numbers(void);
 
 #endif
