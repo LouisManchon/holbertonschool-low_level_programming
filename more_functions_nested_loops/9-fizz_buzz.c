37c405e4929254a0881631f032caf539715a0552
LouisManchon <manchon.louis31@gmail.com>
2025-02-25 15:20:34 +0100

    prints the numbers from 1 to 100 with Buzz, Fizz and FizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,20 +12,20 @@ int main(void)
 
 	for (i = 1 ; i < 101 ; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 && 5 == 0)
 		{
 			printf(" ");
-			printf("Fizz");
+			printf("FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
 			printf(" ");
 			printf("Buzz");
 		}
-		else if (i % 3 && 5 == 0)
+		else if (i % 3 == 0)
 		{
 			printf(" ");
-			printf("FizzBuzz");
+			printf("Fizz");
 		}
 		else
 		{
