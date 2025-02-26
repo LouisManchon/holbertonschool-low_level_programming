41a6485b99aa55cff93484d9e77d1f43c65e7b2d
LouisManchon <manchon.louis31@gmail.com>
2025-02-26 16:47:45 +0100

    Write a function that prints a string, in reverse, followed by a new line

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * print_rev -  prints a string, in reverse, followed by a new line
+ *@s: string of the task
+ */
+
+void print_rev(char *s)
+{
+	int length;
+
+	for (length = _strlen(s) ; length >= 0 ; length--)
+	{
+		_putchar(s[length]);
+	}
+	_putchar('\n');
+}
